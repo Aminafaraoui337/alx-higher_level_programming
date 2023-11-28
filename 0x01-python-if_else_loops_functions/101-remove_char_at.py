75bf646a0d60dc4e1b5e4d8a545b5020175a64a5
Aminafaraoui337 <farraouiamina@gmail.com>
2023-11-28 08:15:13 -0800

    remove c in n

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     wantedstr = ""
-    for i,c in enumerate(str):
+    for i, c in enumerate(str):
         if i != n:
             wantedstr += c
     return wantedstr

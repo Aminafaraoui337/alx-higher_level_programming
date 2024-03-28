ea72c71cd6c17a0d061b78b2e873a004d137f736
Aminafaraoui337 <farraouiamina@gmail.com>
2024-03-27 19:31:37 -0700

    network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""script for finding peak in list of ints,  interview prep """
+"""script for finding peak in list of ints,  interview prep
+"""
 def find_peak(list_of_integers):
+    """BRUTE force implementation for question
+    """
     max_i = None
     for ele in list_of_integers:
         if max_i is None or max_i < ele:
